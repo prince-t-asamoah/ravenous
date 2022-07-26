0949a2ecaa4f5bea0650d52966dd417bb9071a1a
prince-t-asamoah <prince.t.asamoah@gmail.com>
2022-07-26 16:22:14 +0000

    Create BusinessList component and render sample Business components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,18 @@
 import React from "react";
 import './BusinessList.css';
 import Business from "../Business/Business";
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+          <div class="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+          </div>
+        );
+    }
+}

bda3e369c390c628b3a12873d96ffc8fe8f7229c
prince-t-asamoah <prince.t.asamoah@gmail.com>
2022-07-26 15:50:41 +0000

    Add a sample business item data

---
+++ src/components/Business/Business.js
@@ -0,0 +1,11 @@
+const business = {
+    imageSrc: '../../public/images/piza.jgp',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
